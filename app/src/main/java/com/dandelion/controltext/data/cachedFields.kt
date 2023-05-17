14733b69408d9803c3636d45612008ad9757dcdd
Danelyan <juggernautcutcut@gmail.com>
2023-05-17 17:25:33 +0300

    remove comment

--- app/src/main/java/com/dandelion/controltext/data/cachedFields.kt
+++ app/src/main/java/com/dandelion/controltext/data/cachedFields.kt
@@ -2,4 +2,4 @@ package com.dandelion.controltext.data
 
 import androidx.compose.runtime.mutableStateListOf
 
-val cachedFields = mutableStateListOf<CommonOptions>(/*TextFieldOptions()*/) // TODO REMOVE!!
+val cachedFields = mutableStateListOf<CommonOptions>()
